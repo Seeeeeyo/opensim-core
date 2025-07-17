986739856b31daf4f3d248e2ad1be68f74b108b9
Seeeeeyo <selimgilon@gmail.com>
2025-07-17 15:29:29 -0600

    tag2

--- OpenSim/Simulation/BufferedMarkersReference.cpp
+++ OpenSim/Simulation/BufferedMarkersReference.cpp
@@ -20,7 +20,6 @@
  * See the License for the specific language governing permissions and        *
  * limitations under the License.                                             *
  * -------------------------------------------------------------------------- */
-
 #include "BufferedMarkersReference.h"
 #include "MarkersReference.h"
 #include <OpenSim/Common/Units.h>
